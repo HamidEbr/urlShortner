675b92545331f73b77457c53e0b927c9ee1c07ad
Hamid Ebrahimi <hamid.ebrahimi@hotmail.com>
2020-08-18 11:57:41 +0430

    initialize empty solution

---
+++ Api.UrlShortner/Controllers/UrlShornerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.UrlShortner.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UrlShornerController : ControllerBase
+    {
+        private readonly ILogger<UrlShornerController> _logger;
+
+        public UrlShornerController(ILogger<UrlShornerController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
